df9129671db51ddd73f2bc137a8923a87a2be39a
samidarko <samidarko@gmail.com>
2015-10-31 19:46:22 +0800

    chore(releasing): Add travis config and semantic release

---
+++ src/index_test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('react-radial-progress', function () {
+  it('it should load without any problems', function () {
+    expect(true).to.be.true;
+  });
+});
